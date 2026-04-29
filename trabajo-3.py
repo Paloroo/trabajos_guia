00c2f3e578a228eebb70b9ff85e34e18539b7d1f
Rodrigo <rodrigoantonioroaroa@gmail.com>
2026-04-29 11:15:15 -0400

    Commits

---
+++ trabajo-3.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese su nombre: ").strip()
+
+if nombre == "":
+    print("Error: El nombre esta vacio.")
+elif nombre.islower() or nombre.isupper():
+    nombre_formateado = nombre.title()
+    print(f"Nombre formateado: {nombre_formateado}")
+else:
+    print(f"Nombre aceptado tal cual: {nombre}")
